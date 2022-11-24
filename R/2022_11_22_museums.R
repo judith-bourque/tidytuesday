a17f3c9f9b0a4528bb8dd1db686325286b30e035
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2022-11-24 00:35:44 -0500

    Add initial code and Rproj

---
+++ R/2022_11_22_museums.R
@@ -0,0 +1,25 @@
+# Get the Data
+
+# Read in with tidytuesdayR package
+# Install from CRAN via: install.packages("tidytuesdayR")
+
+library("tidyverse")
+
+tuesdata <- tidytuesdayR::tt_load('2022-11-22')
+tuesdata <- tidytuesdayR::tt_load(2022, week = 47)
+
+museums <- tuesdata$museums
+
+
+# Tidy data ---------------------------------------------------------------
+
+
+
+# Create graph data -------------------------------------------------------
+
+
+
+
+# Visualize data ----------------------------------------------------------
+
+
