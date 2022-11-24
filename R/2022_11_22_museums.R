1949f79434bc8e4643376d5e726a17a51036017b
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2022-11-24 00:49:33 -0500

    Add data cleaning

--- R/2022_11_22_museums.R
+++ R/2022_11_22_museums.R
@@ -10,9 +10,19 @@ tuesdata <- tidytuesdayR::tt_load(2022, week = 47)
 
 museums <- tuesdata$museums
 
-
 # Tidy data ---------------------------------------------------------------
 
+museums_tidy <- museums %>%
+  janitor::clean_names() %>%
+  na.omit() %>%
+  separate(., governance, c("governance_cat", "governance_subcat"), sep = "-") %>%
+  mutate(
+    governance_cat = str_to_lower(governance_cat),
+    governance_subcat = str_to_lower(governance_subcat)
+  ) %>%
+  mutate(
+    governance_subcat = gsub("_", " ", governance_subcat)
+  )
 
 
 # Create graph data -------------------------------------------------------
