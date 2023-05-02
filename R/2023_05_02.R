8e98c4dc262bb8d1527fb02cb7808065de06315b
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-05-02 14:09:22 -0400

    Create graph for 2023-05-02 (#2)
    * Add Rproj
    
    * Import data
    
    * Create basic point graph
    
    * Colour granivore
    
    * Add gghighlight
    
    * Add x and y titles
    
    * Create categorical variable
    
    * Add label and highlight
    
    * Add labels, stat_smooth()
    
    * Remove confidence interval
    
    * Add scale
    
    * Axis to grey
    
    * Change scale
    
    * Add caption
    
    * Add source
    
    * Save graph
    
    * Add readme.rmd

---
+++ R/2023_05_02.R
@@ -0,0 +1,41 @@
+# Get the Data
+
+# Read in with tidytuesdayR package
+# Install from CRAN via: install.packages("tidytuesdayR")
+# This loads the readme and all the datasets for the week of interest
+
+# Either ISO-8601 date or year/week works!
+
+library("tidyverse")
+library("gghighlight")
+
+tuesdata <- tidytuesdayR::tt_load('2023-05-02')
+
+plots <- tuesdata$plots
+species <- tuesdata$species
+surveys <- tuesdata$surveys
+
+data <- species %>%
+  mutate(diet = case_when(granivore == 1 ~ "granivore",
+                          .default = as.character("unspecified")))
+
+ggplot(data, aes(x = meanhfl, y = meanwgt, group = diet, colour = diet)) +
+  geom_point() +
+  stat_smooth(method = "lm", se = FALSE) +
+  xlab("Mean weight") +
+  ylab("Mean hindfoot length") +
+  labs(title = "A Rodent's Diet",
+       subtitle = "Granivores tend to be heavier and shorter than rodents with other diet types.",
+       caption = "Data: The Portal Project\nGraphic: github.com/judith-bourque") +
+  scale_colour_discrete(type = c("darkorange", "grey")) +
+  theme_classic() +
+  theme(
+    axis.ticks = element_line(colour = "grey"),
+    axis.line = element_line(colour = "grey"))
+
+ggsave(
+  "graph/2023-05-02.png",
+  width = 7,
+  height = 5,
+  units = "in"
+)
