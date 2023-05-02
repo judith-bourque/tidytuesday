08bb244938b2ef13864261c43ceebfd5108bd266
Judith Bourque <59173478+judith-bourque@users.noreply.github.com>
2023-05-02 12:32:24 -0400

    Create basic point graph

--- R/2023_05_02.R
+++ R/2023_05_02.R
@@ -6,10 +6,15 @@
 
 # Either ISO-8601 date or year/week works!
 
+library("tidyverse")
+
 tuesdata <- tidytuesdayR::tt_load('2023-05-02')
-tuesdata <- tidytuesdayR::tt_load(2023, week = 18)
 
 plots <- tuesdata$plots
 species <- tuesdata$species
 surveys <- tuesdata$surveys
 
+data <- species
+
+ggplot(data, aes(x = meanhfl, y = meanwgt))+
+  geom_point()
